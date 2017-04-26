e65c0ab3158b8094c0c8f86041c7d8065bd2e0c5
Kiran Kumar Talapaku <kiran.talapaku@gmail.com>
2017-04-26 17:59:07 -0500

    fixing view

--- server/views/freshmarket_services.js
+++ server/views/freshmarket_services.js
@@ -1,9 +1,18 @@
+// angular.module('freshMarket.services', [])
+// .factory('$paymentservice', function() {
+//   return {
+//     getToken: function() {
+//       return 'sandbox_dpkzjg9m_2t62rpghphf9nmjv';
+//     }
+//   };
+// });
+ 
+
 angular.module('freshMarket.services', [])
 .factory('$paymentservice', function() {
   return {
     getToken: function() {
-      return 'sandbox_dpkzjg9m_2t62rpghphf9nmjv';
+      return '<%= clientToken %>';
     }
   };
 });
- 
